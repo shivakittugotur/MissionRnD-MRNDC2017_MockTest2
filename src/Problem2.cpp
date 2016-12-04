cccf866cb1290f497e243bc92f11dcadad67d7b9
shivakittugotur <shivakittugotur@missionrnd.com>
2016-12-04 11:30:37 +0530

    Submit Commit

--- src/Problem2.cpp
+++ src/Problem2.cpp
@@ -46,9 +46,63 @@ Constraints :
 
 #include <stdio.h>
 #include <stdlib.h>
+#include<string.h>
 
+void rrotatematrix(int**a, int l){
+	int **b = (int**)malloc(l*sizeof(int*));
+	for (int i = 0; i < l; i++){
+		b[i] = (int*)malloc(l*sizeof(int));
+	}
+	for (int i = l - 1; i >= 0; i--)
+		for (int j = 0; j<l; j++)
+			b[j][l - i - 1] = a[i][j];
+	for (int i = 0; i < l; i++){
+		for (int j = 0; j < l; j++){
+			a[i][j] = b[i][j];
+		}
+	}
 
+}
+void lrotateMatrix(int **a, int l)
+{
+	for (int x = 0; x < l / 2; x++){
+		for (int y = x; y < l - x - 1; y++){
+			int t = a[x][y];
+			a[x][y] = a[y][l - 1 - x];
+			a[y][l - 1 - x] = a[l - 1 - x][l - 1 - y];
+			a[l - 1 - x][l - 1 - y] = a[l - 1 - y][x];
+			a[l - 1 - y][x] = t;
+		}
+	}
+}
 void rotateArray(int **arr, int len, char *seq)
 {
 	//Manipulate input array for output
+	if (len < 500){
+		if (arr == NULL || len <= 0)
+			return ;
+		int c1 = 0, c2 = 0;
+		for (int i = 0; i < strlen(seq); i++){
+			if (seq[i] == 'l' || seq[i] == 'L')
+				c1++;
+			else if (seq[i] == 'r' || seq[i] == 'R')
+				c2++;
+
+		}
+		if (c1 == c2)
+			return;
+		if (c1 > c2){
+			for (int i = 0; i < c1 - c2; i++){
+				lrotateMatrix(arr, len);
+			}
+		}
+		else{
+			for (int i = 0; i < c2 - c1; i++){
+				rrotatematrix(arr, len);
+			}
+		}
+
+
+	}
+
 }
--- src/Problem3.cpp
+++ src/Problem3.cpp
@@ -36,9 +36,87 @@ struct node
 	struct node *prev;
 	struct node *next;
 };
-
+int fact(int n){
+	if (n == 1)
+		return 1;
+	else
+		return n*fact(n - 1);
+}
+int countdigits(int num){
+	int i;
+	while (num != 0){
+		i++;
+		num /= 10;
+	}
+	return i;
+}
+void swap(int *xp, int *yp)
+{
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+void bubbleSort(int arr[], int n)
+{
+	int i, j;
+	for (i = 0; i < n - 1; i++)  
+		for (j = 0; j < n - i - 1; j++)
+			if (arr[j] > arr[j + 1])
+				swap(&arr[j], &arr[j + 1]);
+}
+int countmax(int *arr, int c, int a){
+	int c1= 0;
+	for (int i = 0; i < c; i++){
+		if (arr[i]>a)
+			c1++;
+	}
+	return c1;
+}
 
 int doorstoCross(struct node *passKey)
 {
-	return -1;
+	int c = 0;
+	struct node* curr;
+	curr = passKey;
+	int val = 0;
+	while (curr != NULL){
+		val = val * 10 + curr->num;
+		curr = curr->next;
+	}
+	
+	c = countdigits(val);
+	int * arr1 = (int*)malloc(c*sizeof(int));
+	int * arr2 = (int*)malloc(c*sizeof(int));
+	curr = passKey;
+	int i = 0;
+	while (curr != NULL){
+		arr1[i] = curr->num;
+		arr2[i++] = curr->num;
+		curr = curr->next;
+	}
+	if (c > 12)
+		return -1;
+	bubbleSort(arr2, c);
+	int m = 0, n = 0, k = 0, g = c;;
+	for (k = 0; k < c; k++){
+		for (m = 0; m < c&&arr2[m]>arr1[k]; m++){
+			int p = countmax(arr2, c, arr2[m]);
+			n = n + p*g - 1;
+		}
+		for (int i = 0; i < c; i++){
+			if (arr2[i] != arr1[k]){
+				i++;
+			}
+			else{
+				for (int j = i; j + 1 < c; j++){
+					arr2[j] = arr2[j + 1];
+				}
+				g--;
+				break;
+			}
+		}
+	}
+	return n+1;
+
+	
 }
